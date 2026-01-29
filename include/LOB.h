0cd30ac391646eed9798474e65178e372ee9666b
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2026-01-29 21:58:06 +0000

    Improve LOB and add test.

--- include/LOB.h
+++ include/LOB.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <climits>
 #include <cstdint>
 #include <vector>
 #define ORDERS_NUM 10000
@@ -28,16 +27,44 @@ struct OrderTable {
   container::vector<LevelIdx> levels;
   container::vector<OrderIdx> prev;
   container::vector<OrderIdx> next;
+  container::vector<OrderIdx> free_next;
+  container::vector<OrderIdx> find_index;
+  OrderIdx free_head = UINT64_MAX;
 
   OrderIdx insert() {
-    OrderIdx index = static_cast<OrderIdx>(ids.size());
-    ids.push_back({});
-    quantities.push_back(0);
-    timestamps.push_back(0);
-    levels.push_back(UINT64_MAX);
-    prev.push_back(UINT64_MAX);
-    next.push_back(UINT64_MAX);
+    OrderIdx index;
+    find_index.push_back(UINT64_MAX);
+
+    // Option 1: Reuse free space
+    if (free_head != UINT64_MAX) {
+      index = free_head;
+      free_head = free_next[index];
+    } else {
+      // Option 2: Insert new row
+      index = static_cast<OrderIdx>(ids.size());
+      ids.push_back({});
+      quantities.push_back(0);
+      timestamps.push_back(0);
+      levels.push_back(UINT64_MAX);
+      prev.push_back(UINT64_MAX);
+      next.push_back(UINT64_MAX);
+      free_next.push_back(UINT64_MAX);
+    }
     return index;
+  }
+
+  void remove(OrderId id) {
+    auto index = find_index[id];
+    find_index[id] = UINT64_MAX;
+
+    ids[index] = {};
+    quantities[index] = 0;
+    timestamps[index] = 0;
+    levels[index] = UINT64_MAX;
+    prev[index] = UINT64_MAX;
+    next[index] = UINT64_MAX;
+    free_next[index] = free_head;
+    free_head = index;
   }
 };
 
@@ -86,6 +113,7 @@ inline OrderId insert_order(LOB &lob, Side side, Price price, Quantity qty,
   auto order_id = lob.next_order_id++;
   auto order_index = lob.order_table.insert();
   lob.order_table.ids[order_index] = order_id;
+  lob.order_table.find_index[order_id] = order_index;
   lob.order_table.quantities[order_index] = qty;
   lob.order_table.timestamps[order_index] = ts;
   lob.order_table.levels[order_index] = level_idx;
@@ -105,11 +133,28 @@ inline OrderId insert_order(LOB &lob, Side side, Price price, Quantity qty,
   return order_id;
 }
 
-inline OrderIdx find_order_index(const LOB &lob, OrderId id) {
-  for (OrderIdx index = 0; index < lob.order_table.ids.size(); ++index)
-    if (lob.order_table.ids[index] == id)
-      return index;
+inline void remove_order(LOB &lob, OrderId order_id) {
+  auto order_index = lob.order_table.find_index[order_id];
+  auto level_idx = lob.order_table.levels[order_index];
+  auto qty = lob.order_table.quantities[order_index];
 
-  return UINT64_MAX;
+  auto prev_idx = lob.order_table.prev[order_index];
+  auto next_idx = lob.order_table.next[order_index];
+
+  if (prev_idx != UINT64_MAX)
+    lob.order_table.next[prev_idx] = next_idx;
+  else
+    lob.pl_table.heads[level_idx] = next_idx;
+
+  if (next_idx != UINT64_MAX)
+    lob.order_table.prev[next_idx] = prev_idx;
+  else
+    lob.pl_table.tails[level_idx] = prev_idx;
+
+  lob.pl_table.quantities[level_idx] -= qty;
+  lob.pl_table.order_counts[level_idx]--;
+
+  lob.order_table.remove(order_id);
 }
+
 } // namespace mose
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,22 @@
 
 #include "LOB.h"
+#include <cassert>
 #include <iostream>
-#include <string_view>
 
 #define VERSION 0.1
 
 int main(int argc, char **argv) {
   mose::LOB lob;
-  mose::insert_order(lob, mose::Side::bid, 100.123, 10, 0);
+  auto id1 = mose::insert_order(lob, mose::Side::bid, 100.123, 10, 0);
+  auto id2 = mose::insert_order(lob, mose::Side::bid, 100, 100, 0);
+  auto index2 = lob.order_table.find_index[id2];
 
-  std::cout << lob.pl_table.prices[0] << std::endl;
+  assert(lob.order_table.ids.size() == 2 && lob.pl_table.prices.size() == 2);
+
+  mose::remove_order(lob, id1);
+
+  assert(lob.order_table.ids.size() == 1 && lob.pl_table.prices.size() == 2);
+  assert(lob.order_table.levels[index2] == float(100));
 
   return 0;
 }
