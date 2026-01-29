1e873f13cfdb783012828eb29643af1384945a7f
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2026-01-29 00:00:55 +0000

    Make progress on LOB.

--- include/LOB.h
+++ include/LOB.h
@@ -34,9 +34,9 @@ struct OrderTable {
     ids.push_back({});
     quantities.push_back(0);
     timestamps.push_back(0);
-    levels.push_back(INT_MAX);
-    prev.push_back(INT_MAX);
-    next.push_back(INT_MAX);
+    levels.push_back(UINT64_MAX);
+    prev.push_back(UINT64_MAX);
+    next.push_back(UINT64_MAX);
     return index;
   }
 };
@@ -55,8 +55,8 @@ struct PriceLevelTable {
     sides.push_back(Side::ask);
     quantities.push_back(0);
     order_counts.push_back(0);
-    heads.push_back(INT_MAX);
-    tails.push_back(INT_MAX);
+    heads.push_back(UINT64_MAX);
+    tails.push_back(UINT64_MAX);
     return index;
   }
 };
@@ -64,10 +64,52 @@ struct PriceLevelTable {
 struct LOB {
   OrderTable order_table;
   PriceLevelTable pl_table;
+  OrderId next_order_id = 1;
 };
 
-inline void insert_order(LOB& lob, Side side, Price price, Quantity qty){
+inline OrderId insert_order(LOB &lob, Side side, Price price, Quantity qty,
+                            Timestamp ts) {
+  LevelIdx level_idx = UINT64_MAX;
+  for (LevelIdx i = 0; i < lob.pl_table.prices.size(); ++i) {
+    if (lob.pl_table.prices[i] == price && lob.pl_table.sides[i] == side) {
+      level_idx = i;
+      break;
+    }
+  }
 
+  if (level_idx == UINT64_MAX) {
+    level_idx = lob.pl_table.insert();
+    lob.pl_table.prices[level_idx] = price;
+    lob.pl_table.sides[level_idx] = side;
+  }
+
+  auto order_id = lob.next_order_id++;
+  auto order_index = lob.order_table.insert();
+  lob.order_table.ids[order_index] = order_id;
+  lob.order_table.quantities[order_index] = qty;
+  lob.order_table.timestamps[order_index] = ts;
+  lob.order_table.levels[order_index] = level_idx;
+
+  if (lob.pl_table.heads[level_idx] == UINT64_MAX) {
+    lob.pl_table.heads[level_idx] = order_index;
+    lob.pl_table.tails[level_idx] = order_index;
+  } else {
+    OrderIdx old_tail = lob.pl_table.tails[level_idx];
+    lob.order_table.next[old_tail] = order_index;
+    lob.order_table.prev[order_index] = old_tail;
+    lob.pl_table.tails[level_idx] = order_index;
+  }
+
+  lob.pl_table.quantities[level_idx] += qty;
+  lob.pl_table.order_counts[level_idx]++;
+  return order_id;
 }
 
+inline OrderIdx find_order_index(const LOB &lob, OrderId id) {
+  for (OrderIdx index = 0; index < lob.order_table.ids.size(); ++index)
+    if (lob.order_table.ids[index] == id)
+      return index;
+
+  return UINT64_MAX;
+}
 } // namespace mose
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@
 #define VERSION 0.1
 
 int main(int argc, char **argv) {
-  if (argc > 1 && std::string_view(argv[1]) == "--version")
-    std::cout << "mose version: " << VERSION << std::endl;
+  mose::LOB lob;
+  mose::insert_order(lob, mose::Side::bid, 100.123, 10, 0);
+
+  std::cout << lob.pl_table.prices[0] << std::endl;
 
   return 0;
 }
